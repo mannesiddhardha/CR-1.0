387cca3ab036e3811c553dd0dda5b119b3a93868
manne siddhardha <mannesiddhardha@mannes-mbp.attlocal.net>
2017-11-13 20:41:44 -0500

    amend 1.1

--- python/htmlstatus.py
+++
@@ -1,6 +0,0 @@
-def fileparser():
-	fileopen = open("/Users/mannesiddhardha/Desktop/study/python_learning/log files/one.txt", "r")
-	pattern = re.compile(r"(500)")
-	for line in fileopen:
-		result = pattern.findall(line)
-		print result
