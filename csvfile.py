3b9983b8c1df7ec2c0bb5478293d27a1562fad74
siddhardha manne <mannesidhureboot@gmail.com>
2018-03-31 15:07:00 -0400

    Update csvfile.py

--- csvfile.py
+++ csvfile.py
@@ -13,3 +13,15 @@ with open("/Users/mannesiddhardha/Desktop/test.csv",'r') as fr:
 
 #csv.DictReader(fr)
 #output in dictionary format, parse with the keys
+
+
+# 2nd column 2nd row element in a csv file
+
+with open('/Users/mannesiddhardha/Desktop/test.csv','r') as fr:
+	cr = csv.reader(fr)
+	# indexes parse the columns first
+	result = [line[2] for line in cr]
+	# once you have the list go to the specific row number you want
+
+	print result[2]
+	
